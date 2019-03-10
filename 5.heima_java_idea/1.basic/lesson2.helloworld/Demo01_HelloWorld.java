8c303cfc409e113a5e07211ad3c491406335750d
Your Name <you@example.com>
2019-03-11 03:15:54 +0800

    heima java learn

---
+++ 5.heima_java_idea/1.basic/lesson2.helloworld/Demo01_HelloWorld.java
@@ -0,0 +1,8 @@
+//javac Demo01_HelloWorld.java
+//java Demo01_HelloWorld
+//输出:hello
+public class Demo01_HelloWorld{
+	public static void main(String[] args){
+		System.out.println("hello");
+	}
+}
