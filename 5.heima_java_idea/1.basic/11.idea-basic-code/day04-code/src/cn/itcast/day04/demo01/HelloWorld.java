62ca58236841a6d558e65cf357fb8f6489bc52a0
Your Name <you@example.com>
2019-03-12 18:26:58 +0800

    finish arr

--- 5.heima_java_idea/1.basic/11.idea-basic-code/day04-code/src/cn/itcast/day04/demo01/HelloWorld.java
+++ 5.heima_java_idea/1.basic/11.idea-basic-code/day04-code/src/cn/itcast/day04/demo01/HelloWorld.java
@@ -1,9 +1,7 @@
 package cn.itcast.day04.demo01;
 
 public class HelloWorld {
-
     public static void main(String[] args) {
-        System.out.println("Hello, World!111");
+System.out.println("Hello, World!111");
     }
-
 }
