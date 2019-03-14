d9e325af28f3ad1aeb902733cf03f9018d223a59
Your Name <you@example.com>
2019-03-14 14:53:51 +0800

    part1

--- 5.heima_java_idea/1.basic/11.idea-basic-code/day07-code/src/cn/itcast/day07/demo05/test.java
+++
@@ -1,9 +0,0 @@
-package cn.itcast.day07.demo05;
-
-public class test {
-    public static void main(String[] args) {
-        for (int i = 0; i < 6; i++) {
-            System.out.println();
-        }
-    }
-}
