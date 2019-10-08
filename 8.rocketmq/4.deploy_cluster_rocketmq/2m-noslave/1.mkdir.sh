830c531285a35f612f422f487529d94b79035dd3
ybx <87212426@qq.com>
2019-10-08 14:00:50 +0800

    local rocketmq install

--- 8.rocketmq/4.deploy_cluster_rocketmq/2m-noslave/1.mkdir.sh
+++ 8.rocketmq/4.deploy_cluster_rocketmq/2m-noslave/1.mkdir.sh
@@ -1,4 +1,9 @@
 #!/bin/bash
+rm -rf ~/rocketmq/broker-a/logs
+rm -rf ~/rocketmq/broker-b/logs
+rm -rf ~/rocketmq/broker-a/store
+rm -rf ~/rocketmq/broker-b/store
+
 mkdir -p ~/rocketmq/broker-a/logs
 mkdir -p ~/rocketmq/broker-b/logs
 mkdir -p ~/rocketmq/broker-a/store
